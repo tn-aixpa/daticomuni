2c4031dbe7d5da51872f051eb8f2b0c2e173b20b
Nawaz Khurshid <nawaz1981@gmail.com>
2025-01-24 10:30:03 +0100

    Fixes for transformation in to dh platform project

--- src/convert-all.py
+++ src/convert-all.py
@@ -1,32 +1,48 @@
 
-import mlrun
 import pandas as pd
+from os import path, makedirs
+import zipfile
+        
+file_basepath = "daticomuni"
 
-@mlrun.handler()
-def convert_all(context, source_url_base: str):
-    # direct processing, no actions required
+def convert_all(project, source_artifact):
+    data_dir = f"{file_basepath}/data"
+    try:
+        shutil.rmtree(data_dir)
+    except:
+        print("Error deleting data dir")
+                
+    # Create the directory for the data
+    if not path.exists(data_dir):
+        makedirs(data_dir)
+        
+    try:
+        archiveFile =source_artifact.download(data_dir) # this must change in the function
+        with zipfile.ZipFile(archiveFile, 'r') as zip_ref:
+            zip_ref.extractall(data_dir)    
+    except:
+        print("Error downloading data")
+        
+           
     for ds_name in ["azioni", "campi", "macroambiti", "piani", "tassonomia"]:
-        source_url = source_url_base + ds_name + ".txt"
-        input_data = mlrun.get_dataitem(source_url)
-        df = input_data.as_df(format="csv", encoding="windows-1251", delimiter=";")
+        source_url = data_dir + '/' + ds_name + ".txt"        
+        df = pd.read_csv(source_url, encoding="windows-1251", delimiter=";")
         df.reset_index(drop=True, inplace=True)
-        context.log_dataset(ds_name, df=df, index=False)
+        project.log_dataitem(ds_name, data=df, kind='table', index=False)        
 
     # comuni: process name and dates
-    source_url = source_url_base + "comuni" + ".txt"
-    input_data = mlrun.get_dataitem(source_url)
-    df = input_data.as_df(format="csv", encoding="windows-1251", delimiter=";")
+    source_url =  data_dir + '/' + "comuni" + ".txt"
+    df = pd.read_csv(source_url, encoding="windows-1251", delimiter=";")
     df["comune"] = df["NomeOrganizzazione"].str.replace("COMUNE DI ", "").str.upper()
     df["Data_det_assegnazione"] = pd.to_datetime(df["Data_det_assegnazione"], format="%d/%m/%Y %H:%M:%S", errors="ignore")
     df["Data_det_revoca"] = df["Data_det_revoca"].fillna("")
     df["Data_det_revoca"] = pd.to_datetime(df["Data_det_revoca"], format="%d/%m/%Y %H:%M:%S", errors="ignore")
     df.reset_index(drop=True, inplace=True)
-    context.log_dataset("comuni", df=df, index=False)
+    project.log_dataitem("comuni", data=df, kind='table', index=False)
 
     # valutazioni: process dates
-    source_url = source_url_base + "valutazioni" + ".txt"
-    input_data = mlrun.get_dataitem(source_url)
-    df = input_data.as_df(format="csv", encoding="windows-1251", delimiter=";")
+    source_url =  data_dir + '/' + "valutazioni" + ".txt"
+    df = pd.read_csv(source_url, encoding="windows-1251", delimiter=";")
     df["data_pub"] = pd.to_datetime(df["data_pub"], format="%d/%m/%Y %H:%M:%S", errors="ignore")
     df.reset_index(drop=True, inplace=True)
-    context.log_dataset("valutazioni", df=df, index=False)
+    project.log_dataitem("valutazioni", data=df, kind='table', index=False)
--- src/convert-aziendali.py
+++ src/convert-aziendali.py
@@ -1,19 +1,37 @@
 
-import mlrun
 import pandas as pd
 import numpy as np
 
-@mlrun.handler()
-def convert_aziendali(context, source_url_base: str):
+file_basepath = "daticomuni"
+
+def convert_aziendali(project, source_artifact):
     converters={
         '2024_06_25 PIANI AZIENDALI': {'IDorganizzazione': np.int64, 'ANNUALITA': np.int64, 'Versione': np.int64, 'AnnoCompilazione': np.int64, 'CodiceCampoAzione': np.int64, 'CodiceTassonomiaAzione': np.int64, 'BeneF': np.int64, 'BeneM': np.int64, 'IDdettaglioAccorpamento': np.int64},
         'NuovaTassonomia': {},
         'T_NuovaTassonomia_DettaglioRev': {},
     }
 
+    data_dir = f"{file_basepath}/data"
+    try:
+        shutil.rmtree(data_dir)
+    except:
+        print("Error deleting data dir")
+                
+    # Create the directory for the data
+    if not path.exists(data_dir):
+        makedirs(data_dir)
+        
+    try:
+        archiveFile =source_artifact.download(data_dir) # this must change in the function
+        with zipfile.ZipFile(archiveFile, 'r') as zip_ref:
+            zip_ref.extractall(data_dir)    
+    except:
+        print("Error downloading data")
+        
     for ds_name in ["2024_06_25 PIANI AZIENDALI", "NuovaTassonomia", "T_NuovaTassonomia_DettaglioRev"]:
-        source_url = source_url_base + ds_name + ".xlsx"
-        input_data = mlrun.get_dataitem(source_url)
-        df = pd.read_excel(input_data.get(), sheet_name=0, header=0, converters=converters[ds_name])
+        source_url = data_dir + '/' + ds_name + ".xlsx"
+        df = pd.read_csv(source_url, encoding="windows-1251", delimiter=";")
+        # df = pd.read_excel(input_data.get(), sheet_name=0, header=0, converters=converters[ds_name])
         df.reset_index(drop=True, inplace=True)
-        context.log_dataset(ds_name, df=df, index=False)
+         project.log_dataitem(ds_name, data=df, kind='table', index=False)        
+
